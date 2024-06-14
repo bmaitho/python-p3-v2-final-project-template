3d487af4a49633ef59965bc2e6b87252f0d95f99
Brian Maitho <bmaitho@gmail.com>
2024-06-14 13:32:52 +0300

    Added all relevant instances to genre table

--- lib/helpers.py
+++ lib/helpers.py
@@ -1,19 +1,2 @@
 # lib/helpers.py
-from models.song import Song
-def exit_program():
-    print("Goodbye!")
-def add_song():
-    
-    name = input("Enter song name: ")
-    artist=input("Enter artist name: ")
-    genre = input("Enter genre: ")
-    
 
-    try:
-        Song.create_song(name, artist, genre)
-        print("Song added successfully!")
-    except Exception as e:
-        print("An error occurred:", e)
-
-
-    
--- lib/models/genre.py
+++ lib/models/genre.py
@@ -1,6 +1,7 @@
 from models.__init__ import CURSOR, CONN
 
 class Genre:
+    all = {}
     def __init__(self, name,id=None, ):
         self.id = id
         self.name = name
@@ -21,7 +22,7 @@ class Genre:
                 "Name must be a non-empty string"
             )
     @classmethod
-    def create_genre(cls):  
+    def create_table(cls):  
          """ Create a new table to persist the attributes of Genre instances """
          sql = """
             CREATE TABLE IF NOT EXISTS genres (
@@ -38,7 +39,52 @@ class Genre:
             
          CURSOR.execute(sql)
          CONN.commit()
-         
+
+    def save(self):
+        """ Insert a new row with the name value of the current Genre instance.
+        Update object id attribute using the primary key value of new row.
+        Save the object in local dictionary using table row's PK as dictionary key"""
+        sql = """
+            INSERT INTO genres (name)
+            VALUES (?)
+        """
+
+        CURSOR.execute(sql, (self.name))
+        CONN.commit()
+
+        self.id = CURSOR.lastrowid
+        type(self).all[self.id] = self
+
+    def create(cls, name):
+        """ Initialize a new Genre instance and save the object to the database """
+        genre = cls(name)
+        genre.save()
+        return genre     
         
-    
-    
+    def delete(self):
+        """Delete the table row corresponding to the current Genre instance,
+        delete the dictionary entry, and reassign id attribute"""
+
+        sql = """
+            DELETE FROM genres
+            WHERE id = ?
+        """
+
+        CURSOR.execute(sql, (self.id,))
+        CONN.commit()
+
+        del type(self).all[self.id]
+
+        self.id = None
+
+    @classmethod
+    def get_all(cls):
+        """Return a list containing a Genre object per row in the table"""
+        sql = """
+            SELECT *
+            FROM genres
+        """
+
+        rows = CURSOR.execute(sql).fetchall()
+
+        return [cls.instance_from_db(row) for row in rows]
