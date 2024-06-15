9f41569e0ad57cd5b689ba1a53478f7b5f56ccc9
Brian Maitho <bmaitho@gmail.com>
2024-06-15 08:53:35 +0300

    updated seed .py

--- lib/seed.py
+++ lib/seed.py
@@ -3,43 +3,46 @@
 from models.__init__ import CONN, CURSOR
 from models.genre import Genre
 from models.song import Song
+from models.artist import Artist
 
 def seed_database():
     # Drop the tables if they exist and create new ones
     Genre.drop_table()
     Genre.create_table()
+    Artist.drop_table()
+    Artist.create_table()
     Song.drop_table()
     Song.create_table()
 
     # Create seed data for genres
     genres = [
-        "Rock",
-        "Jazz",
-        "Classical",
-        "Hip-Hop",
-        "Electronic",
-        "Country",
-        "Reggae",
-        "Pop",
-        "Blues"
+        "Rock", "Jazz", "Classical", "Hip-Hop", "Electronic",
+        "Country", "Reggae", "Pop", "Blues"
     ]
     for genre_name in genres:
         Genre.create(genre_name)
 
+    # Create seed data for artists
+    artists = [
+        "Led Zeppelin", "Miles Davis", "Ludwig van Beethoven", "Eminem", "Zedd",
+        "John Denver", "Bob Marley", "Michael Jackson", "B.B. King"
+    ]
+    artist_instances = [Artist.create(name) for name in artists]
+
     # Create seed data for songs
     songs = [
-        ("Stairway to Heaven", "Led Zeppelin", 1), 
-        ("So What", "Miles Davis", 2),                       # Jazz
-        ("Moonlight Sonata", "Ludwig van Beethoven", 3),      # Classical
-        ("Lose Yourself", "Eminem", 4),                       # Hip-Hop
-        ("Clarity", "Zedd", 5),                               #   Electronic
-        ("Take Me Home, Country Roads", "John Denver", 6),    # Country
-        ("One Love", "Bob Marley", 7),                        # Reggae
-        ("Thriller", "Michael Jackson", 8),                   #  Pop
-        ("The Thrill Is Gone", "B.B. King", 9)                # Blues
+        ("Stairway to Heaven", artist_instances[0].id, 1), 
+        ("So What", artist_instances[1].id, 2),
+        ("Moonlight Sonata", artist_instances[2].id, 3),
+        ("Lose Yourself", artist_instances[3].id, 4),
+        ("Clarity", artist_instances[4].id, 5),
+        ("Take Me Home, Country Roads", artist_instances[5].id, 6),
+        ("One Love", artist_instances[6].id, 7),
+        ("Thriller", artist_instances[7].id, 8),
+        ("The Thrill Is Gone", artist_instances[8].id, 9)
     ]
-    for title, artist, genre_id in songs:
-        Song.create(title, artist, genre_id)
+    for title, artist_id, genre_id in songs:
+        Song.create(title, artist_id, genre_id)
 
 seed_database()
 print("Seeded database")
